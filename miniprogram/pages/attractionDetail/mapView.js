1a586a91c50a19769db434d0be5099b239760cbc
WxxShirley <WxxShirley@outlook.com>
2020-10-29 01:06:44 +0800

    完善景区腾讯地图界面

--- miniprogram/pages/attractionDetail/mapView.js
+++ miniprogram/pages/attractionDetail/mapView.js
@@ -20,7 +20,17 @@ Page({
     var marker = [{id:1, 
       latitude: obj.lat,
       longitude:obj.lng,
-      address:obj.loc
+      address:obj.loc,
+      callout: {
+        content: obj.loc,  //文本
+        color: '#FF0202',  //文本颜色
+        borderRadius: 3,  //边框圆角
+        borderWidth: 1,  //边框宽度
+        borderColor: '#FF0202',  //边框颜色
+        bgColor: '#ffffff',  //背景色
+        padding: 5,  //文本边缘留白
+        textAlign: 'center'  //文本对齐方式。有效值: left, right, center
+      }
     }]
     
     this.setData({
@@ -42,52 +52,5 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })

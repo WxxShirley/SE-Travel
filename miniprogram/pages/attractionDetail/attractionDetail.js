3677aff57fdfdc53cb228e63adea8da5050e556b
Ruiyang-061X <17717073617@163.com>
2020-11-14 21:50:53 +0800

    景区周边景点及景区导航

--- miniprogram/pages/attractionDetail/attractionDetail.js
+++ miniprogram/pages/attractionDetail/attractionDetail.js
@@ -1,5 +1,6 @@
 // 景区详情界面 
 var data = require("../../utils/staticData");
+const app = getApp()
 
 Page({
   data: {
@@ -27,17 +28,55 @@ Page({
     })
   },
 
-  goMap: function(e){
-    var mapObj = {
-      "lat":this.data.attractionObj.geoInfo.lat,
-      "lng":this.data.attractionObj.geoInfo.lng,
-      "loc":this.data.attractionObj.geoInfo.location
+  goMap: function(e) {
+    var mapObj = {}
+    mapObj['latitude'] = this.data.attractionObj.geoInfo.lat
+    mapObj['longitude'] = this.data.attractionObj.geoInfo.lng
+    var markers = []
+    markers.push(
+      {
+        id: this.data.attractionObj.id,
+        latitude: this.data.attractionObj.geoInfo.lat,
+        longitude: this.data.attractionObj.geoInfo.lng,
+      }
+    )
+    for(var i = 0; i < this.data.attractionObj.nearBy.length; i ++)
+    {
+      app.getInfoWhere(
+        'touristAttraction',
+        {
+          id: this.data.attractionObj.nearBy[i],
+        },
+        res=>{
+          markers.push(
+            {
+              id: res.data[0].id,
+              latitude: res.data[0].geoInfo.lat,
+              longitude: res.data[0].geoInfo.lng,
+            }
+          )
+        }
+      )
     }
+    console.log(markers)
+    mapObj['markers'] = markers
     mapObj = JSON.stringify(mapObj)
     wx.navigateTo({
-      url: '../../pages/attractionDetail/mapView?mapObj='+mapObj,
+      url: '../../pages/attractionDetail/mapView?mapObj=' + mapObj,
     })
-  }
+  },
 
-  
+  goNavigation: function(e) {
+    let plugin = requirePlugin('routePlan');
+    let key = 'G5ABZ-DOAR4-6B2UZ-XBORG-KCVC2-IUBXG';  //使用在腾讯位置服务申请的key
+    let referer = '沪驴';   //调用插件的app的名称
+    let endPoint = JSON.stringify({  //终点
+      'name': this.data.attractionObj.geoInfo.location,
+      'latitude': this.data.attractionObj.geoInfo.lat,
+      'longitude': this.data.attractionObj.geoInfo.lng
+    });
+    wx.navigateTo({
+      url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
+    });
+  },
 })
--- miniprogram/pages/attractionDetail/mapView.js
+++ miniprogram/pages/attractionDetail/mapView.js
@@ -1,56 +1,29 @@
 // 地图界面
 Page({
   data: {
-    title: '详细地址',
-    address:"测试地址",
-    longitude: null, // 初始化经纬度为外滩经纬度
+    longitude: null,
     latitude: null,
-    scale: 12,
     markers: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    //var that = this
-    
-    var obj = JSON.parse(options.mapObj)
-    console.log(obj)
-    var marker = [{id:1, 
-      latitude: obj.lat,
-      longitude:obj.lng,
-      address:obj.loc,
-      callout: {
-        content: obj.loc,  //文本
-        color: '#FF0202',  //文本颜色
-        borderRadius: 3,  //边框圆角
-        borderWidth: 1,  //边框宽度
-        borderColor: '#FF0202',  //边框颜色
-        bgColor: '#ffffff',  //背景色
-        padding: 5,  //文本边缘留白
-        textAlign: 'center'  //文本对齐方式。有效值: left, right, center
+  onLoad: function (options) { 
+    var mapObj = JSON.parse(options.mapObj)
+    this.setData(
+      {
+        latitude: mapObj.latitude,
+        longitude: mapObj.longitude,
+        markers: mapObj.markers,
       }
-    }]
-    
-    this.setData({
-      latitude: obj.lat,
-      longitude:obj.lng,
-      address:obj.loc, 
-      markers: marker
-    })
-    
-    var that = this
-    
-    // 得到用户当前的位置
-    wx.getLocation({
-      type: 'gcj02',
-      success(res){
-        console.log(res)
+    )
+    wx.getLocation(
+      {
+        type: 'gcj02'
       }
-    })
-    
+    )
   },
 
-  
+  goFromMarkerToAttractionDetail: function (e) {
+    var utils = require('../../utils/util')
+    utils.goattrDetail(e.detail)
+  }
 })

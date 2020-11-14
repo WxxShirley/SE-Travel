a0cd22056d1941f789c07d39119157807f5d79e0
WxxShirley <WxxShirley@outlook.com>
2020-11-14 22:38:38 +0800

    fix地图周边景点服务

--- miniprogram/pages/attractionDetail/attractionDetail.js
+++ miniprogram/pages/attractionDetail/attractionDetail.js
@@ -38,9 +38,14 @@ Page({
         id: this.data.attractionObj.id,
         latitude: this.data.attractionObj.geoInfo.lat,
         longitude: this.data.attractionObj.geoInfo.lng,
+        loc: this.data.attractionObj.geoInfo.location,
+        title: this.data.attractionObj.title,
       }
     )
-    for(var i = 0; i < this.data.attractionObj.nearBy.length; i ++)
+    
+    let cnt=1, total_num = Math.min(this.data.attractionObj.nearBy.length,10);
+
+    for(var i = 0; i < total_num; i ++)
     {
       app.getInfoWhere(
         'touristAttraction',
@@ -48,35 +53,28 @@ Page({
           id: this.data.attractionObj.nearBy[i],
         },
         res=>{
+          console.log(markers)
+          cnt++
           markers.push(
             {
               id: res.data[0].id,
               latitude: res.data[0].geoInfo.lat,
               longitude: res.data[0].geoInfo.lng,
+              loc: res.data[0].geoInfo.location,
+              title: res.data[0].title,
             }
           )
+          if(total_num <= cnt){
+            mapObj['markers'] = markers
+            mapObj['id'] = this.data.attractionObj.id
+            mapObj = JSON.stringify(mapObj)
+            wx.navigateTo({
+              url: '../../pages/attractionDetail/mapView?mapObj=' + mapObj,
+             })
+          }
         }
       )
     }
-    console.log(markers)
-    mapObj['markers'] = markers
-    mapObj = JSON.stringify(mapObj)
-    wx.navigateTo({
-      url: '../../pages/attractionDetail/mapView?mapObj=' + mapObj,
-    })
-  },
-
-  goNavigation: function(e) {
-    let plugin = requirePlugin('routePlan');
-    let key = 'G5ABZ-DOAR4-6B2UZ-XBORG-KCVC2-IUBXG';  //使用在腾讯位置服务申请的key
-    let referer = '沪驴';   //调用插件的app的名称
-    let endPoint = JSON.stringify({  //终点
-      'name': this.data.attractionObj.geoInfo.location,
-      'latitude': this.data.attractionObj.geoInfo.lat,
-      'longitude': this.data.attractionObj.geoInfo.lng
-    });
-    wx.navigateTo({
-      url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
-    });
+    
   },
 })
--- miniprogram/pages/attractionDetail/mapView.js
+++ miniprogram/pages/attractionDetail/mapView.js
@@ -3,16 +3,33 @@ Page({
   data: {
     longitude: null,
     latitude: null,
-    markers: []
+    markers: [],
+    id: null
   },
 
   onLoad: function (options) { 
     var mapObj = JSON.parse(options.mapObj)
+
+    for(var i=0;i<mapObj.markers.length;i++){
+      var marker = mapObj.markers[i];
+      marker["callout"] = {
+        content: marker.title + ',' +marker.loc,
+        color: '#FF0202',
+        borderRadius: 3,
+        borderWidth: 1,
+        borderColor: '#FF0202',
+        bgColor: '#ffffff',
+        padding: 5,
+        textAlign: 'center'
+      }
+    }
+
     this.setData(
       {
         latitude: mapObj.latitude,
         longitude: mapObj.longitude,
         markers: mapObj.markers,
+        id: mapObj.id
       }
     )
     wx.getLocation(
@@ -24,6 +41,8 @@ Page({
 
   goFromMarkerToAttractionDetail: function (e) {
     var utils = require('../../utils/util')
-    utils.goattrDetail(e.detail)
+    console.log(e.markerId)
+    if(e.markerId!=this.data.id)
+        utils.goattrDetail(e.markerId)
   }
 })

cdfd480b69a29e6001b9db85a39888170e5b4fad
veghen <17307130354@fudan.edu.cn>
2020-11-14 20:07:19 +0800

    瀑布流攻略展示

---
+++ cloudfunctions/getEntry/index.js
@@ -0,0 +1,23 @@
+/* 
+    模版函数：增加数据库中的若干条记录路记录
+    @param event (event.collection -> 表名, event.skip -> 开始获取的地方, event.num -> 获取条数)
+*/
+
+const cloud = require('wx-server-sdk')
+
+cloud.init({env: 'env-dev-6gb5dffd859b69ee'})
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let openid = cloud.getWXContext().OPENID
+  
+  entry.openid = openid
+  
+  try{
+    return await db.collection(event.collection).skip(event.skip).limit(event.num).get()
+  }catch(e){
+    console.log(e)
+    return e
+  }
+}
--- miniprogram/components/findPage/findPage.js
+++ miniprogram/components/findPage/findPage.js
@@ -1,3 +1,5 @@
+var mockData = require("../../models/mockData")
+
 // components/findPage/findPage.js
 Component({
   /**
@@ -12,17 +14,74 @@ Component({
    */
   data: {
     active: 0,
+    guideData: [],
+    leftList: [],
+    rightList: [],
   },
 
   /**
    * 组件的方法列表
    */
+  lifetimes:{
+    attached: function(){
+      var leftData = []
+      var rightData = []
+      this.getGuideData(0, 20)
+      
+      for(let i=0;i<4;i++){
+          var d = this.guideData[i]
+          d.amILike = false
+  
+          if(i%2==0){
+            leftData.push(d)
+          }else{
+            rightData.push(d)
+          }
+        }
+     
+      this.setData({
+        leftList: leftData,
+        rightList: rightData,
+      })
+    }
+  }, 
+
   methods: {
     onChange: function(event){
-      wx.showToast({
-        title: `切换到标签 ${event.detail.name}`,
-        icon: 'success',
-      });
-    }
+      // wx.showToast({
+      //   title: `切换到标签 ${event.detail.name}`,
+      //   icon: 'success',
+      // });
+      console.log(event.detail+1)
+      this.setData({active: event.detail.name});
+    },
+
+    getGuideData: function(skip, num){
+      const db = wx.cloud.database()
+      var that = this
+      db.collection("guide").skip(skip).limit(num).get().then(res=>{
+        console.log(res)
+        that.setData({
+          guideData: res.data
+        })
+        wx.showToast({
+          title: `res${res.data[0].title}`,
+          icon: 'success',
+        })
+      })
+      // wx.cloud.callFunction({
+      //   name: 'getEntry',
+      //   data: {collection: 'guide', skip: skip, num: num},
+      //  }).then(res=>{
+      //   console.log(res)
+      //   this.setData({
+      //     guideData: res.result.data
+      //   })
+      //   wx.showToast({
+      //     title: `res${res.result.data.length}`,
+      //     icon: 'success',
+      //   })
+      // })
+    },
   }
 })
